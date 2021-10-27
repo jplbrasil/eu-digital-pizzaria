95c1c9f1b27ae1ff653980e9cb5eefcdafb70929
jplbrasil <52611406+jplbrasil@users.noreply.github.com>
2021-10-27 18:37:31 -0300

    Add files via upload

--- incluir.php
+++ incluir.php
@@ -15,11 +15,20 @@
 		<hr>		
 	
 		<?php
+			/* Dados para conexao em base de dados local
 			$servername = "localhost";
 			$username = "root";
 			$password = "";
 			$dbname = "pizzaria";
+			//*/
 
+			///* Dados para conexao 'a base de dados da nuvem Heroku, aplicativo eu-digital-pizzaria
+			$servername = "us-cdbr-east-04.cleardb.com";
+			$username = "bff01010138ab1";
+			$password = "e04e9d48";
+			$dbname = "heroku_264338b10d713e0";
+			//*/
+				
 			// Create connection
 			$conn = new mysqli($servername, $username, $password, $dbname);
 
