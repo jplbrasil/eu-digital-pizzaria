6f3f24ce7172c449abea3b03b021e63cf1906c61
jplbrasil <52611406+jplbrasil@users.noreply.github.com>
2021-11-19 19:06:54 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -11,17 +11,18 @@
 	<body>
 
 		<center>
-		<img src="imagens/principal.jpg"> 
+		<!-- <img src="imagens/principal.jpg">
 		<br>
-		<h2>Pizzaria Brasileira</h2>
+		-->
+		<h1>Pizzaria Brasileira</h1>
 				
 		<?php
 			
 			session_start();
 			
 			if(isset($_SESSION['cliente'])) {
-				echo "Olá, " . $_SESSION['cliente'] . " &nbsp <a href='cliente/logoff.php'>(sair)</a>\n";
-				echo " &nbsp &nbsp <a href='pedido/carrinho.php'>(meu carrinho) <img width='2%' src='imagens/carrinho.png'> </a>\n";
+				echo "Olá, " . strtok($_SESSION['cliente']," ") . " &nbsp <a href='cliente/logoff.php'>sair</a>\n";
+				echo " &nbsp &nbsp <a href='pedido/carrinho.php'>meu carrinho <img width='2%' src='imagens/carrinho.png'> </a>\n";
 			}
 			else {
 
@@ -32,7 +33,7 @@
 				echo "<td>E-mail: <input type='text' name='email'></td>\n";
 				echo "<td>Senha: <td><input type='password' name='senha'></td>\n";
 				echo "<td><input type='submit' value='Entrar'></td>\n";
-				echo "<td><a href='cliente/cadastrar.php'>Ainda não sou cadastrado(a)</a></td>\n";
+				echo "<td> &nbsp &nbsp <a href='cliente/cadastrar.php'>Ainda não sou cadastrado(a)</a></td>\n";
 				echo "</tr>\n";
 				echo "</table>\n";			
 				echo "</form>\n";
@@ -48,64 +49,96 @@
 	
 		</center>
 		
-		<br>
+		<br> <br>
+
+		<table border=1 align="center" style="width:80%">
 		
-		<div id="produtos" style="overflow:auto;width:100%;height:200px">
-		
-		<table border=0 align="center">
-	
-			<thead>
-	
-			<tr bgcolor="#DDDDDD">
-				<th>sabor</th>
-				<th>imagem</th>				
-				<th>ingredientes</th>
-				<th>tamanho</th>
-				<th>preço (R$)</th>
-				
-				<?php
-					if(isset($_SESSION['cliente']))
-						echo "<th>incluir no pedido</th>\n";
-				?>
-			</tr>
+			<tr>
+
+				<td align='center' style='width:10%'>
+				<a href='index.php?filtro=1&categoria=Todas'>Todos Produtos</a>
+				</td>
 			
-			</thead>
+				<?php
+				
+					$sql = "SELECT DISTINCT(categoria) FROM produto";
+					$result = $conn->query($sql);
+
+					if ($result->num_rows > 0) {
+
+						while($row = $result->fetch_assoc()) {
+					
+							echo "<td align='center' style='width:10%'>\n";
+							echo "<a href='index.php?filtro=1&categoria=" . $row["categoria"] . "'> ". $row["categoria"] . "</a>\n";
+							echo "</td>\n";
+						}
+					}
+				
+				?>
+			
+			</tr>
+		
+		</table>
+		
+		
+		<div id="produtos" style="overflow:auto;width:100%;height:400px">
+		
+		<table border=0 align="center" style="width:80%">
 			
 			<tbody>
 			
 			<?php
 
-				$sql = "SELECT * FROM produto";
+				if(!isset($_REQUEST["filtro"]))
+					$sql = "SELECT * FROM produto";
+				else {
+					if($_REQUEST["categoria"] == "Todas")
+						$sql = "SELECT * FROM produto";
+					else
+						$sql = "SELECT * FROM produto WHERE categoria = '" . $_REQUEST["categoria"] . "'";
+				}
+				
 				$result = $conn->query($sql);
 
 				if ($result->num_rows > 0) {
 
+					$colunas = 0;
+				
 					while($row = $result->fetch_assoc()) {
+												
+						if(($colunas % 3) == 0)
+							echo "<tr>\n";
+							
+						$colunas ++;						
 						
-						//Esse form e' para permitir ao cliente incluir produtos no carrinho
-						echo "<form action='pedido/carrinho.php'>\n";
-						
-						echo "<tr>\n";
-						echo "<td align='center'>" . $row["sabor"] . "</td>\n";
-						echo "<td align='center'>" . "<img width='50%' src='imagens/sabores/". $row["imagem"] . "'></td>\n";
-						echo "<td align='center'>" . $row["ingredientes"] . "</td>\n";
-						echo "<td align='center'>" . $row["tamanho"] . "</td>\n";
-						echo "<td align='center'>" . $row["preco"] . "</td>\n";
-					
+						echo "<td align='center' style='width:33%'>\n";
+						echo "<br><br>\n";
+						echo "<img width='40%' src='imagens/". $row["imagem"] . "'>\n";
+						echo "<br><br>\n";
+						echo "<b>" . $row["titulo"] . "</b>\n";
+						echo "<br><br>\n";
+						echo "<textarea rows=3 cols=50 readonly style='resize:none'>" . $row["descricao"] . "</textarea>\n";
+						echo "<br><br>\n";
+						printf("<b> R$ %01.2f </b>\n", $row["preco"]);
+
+						//Esse form e' para permitir ao cliente incluir produtos no carrinho						
 						if(isset($_SESSION['cliente'])) {
-							echo "<td align='center'>\n";
+							echo "<br><br>\n";
+							echo "<form action='pedido/carrinho.php'>\n";
 							echo "<input type='hidden' name='idProduto' value=" . $row["id"] . ">\n";
 							echo "<input type='submit' value='Incluir'>\n";
-							echo "</td>\n";
+							echo "</form>\n";
 						}
+
+						echo "</td>\n";
+
+						if(($colunas % 3) == 0)
+							echo "</tr>\n";
 						
-						echo "</tr>\n";
-						
-						echo "</form>\n";
 					}
-			
-				} else {
-					//echo "0 results";
+					
+					if(($colunas % 3) != 0)
+						echo "</tr>\n";
 				}
 			
 				$conn->close();
@@ -117,12 +150,14 @@
 		
 		<!-- fecha a div produtos -->
 		</div>
-				
+						
 		<br>
 		
 		<center>
 		<p>Endereço: Rua Abcd Xyz, 900. Centro, Contagem. Telefone / WhatsApp: (31) 3333-4444</p>
 		</center>
+		
+		<!--
 		
 		<br>
 		
@@ -135,6 +170,8 @@
 		<center>
 		<a href="cliente/cadastro.php">Cadastro de Clientes</a>
 		</center>
+		
+		-->
 
 	</body>
 	
