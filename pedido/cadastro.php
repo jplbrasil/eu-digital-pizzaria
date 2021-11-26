c9773f01024617a27cbd13255466f35c6355b2da
jplbrasil <52611406+jplbrasil@users.noreply.github.com>
2021-11-26 16:28:55 -0300

    Add files via upload

--- pedido/cadastro.php
+++ pedido/cadastro.php
@@ -13,7 +13,16 @@
 		<table border=1 align="center">
 	
 			<tr>
-				<th>Id</th>
+				<th>Id do Pedido</th>
+				<th>Data</th>
+				<th>Hora</th>
+				<th>Id Cliente</th>
+				<th>Tipo de Pagamento</th>
+				<th>Tipo de Entrega</th>
+				<th>Valor dos Produtos</th>
+				<th>Valor do Frete</th>
+				<th>Valor Total</th>
+				<th>Status</th>
 				<th>Ação</th>
 			</tr>
 			
@@ -27,6 +36,15 @@
 					while($row = $result->fetch_assoc()) {
 						echo "<tr>\n";
 						echo "<td>" . $row["id"] . "</td>\n";
+						echo "<td>" . $row["data"] . "</td>\n";
+						echo "<td>" . $row["hora"] . "</td>\n";
+						echo "<td>" . $row["id_cliente"] . "</td>\n";
+						echo "<td>" . $row["tipo_pagamento"] . "</td>\n";
+						echo "<td>" . $row["tipo_entrega"] . "</td>\n";
+						echo "<td>" . $row["valor_produtos"] . "</td>\n";
+						echo "<td>" . $row["valor_frete"] . "</td>\n";
+						echo "<td>" . $row["valor_total"] . "</td>\n";
+						echo "<td>" . $row["status"] . "</td>\n";
 						echo "<td><a href='excluir.php?id=" . $row["id"] . "'>&nbsp &nbsp Excluir &nbsp &nbsp</a></td>\n";
 						echo "</tr>\n";
 					}
