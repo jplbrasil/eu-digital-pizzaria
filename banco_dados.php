7ba219d407bd9ad2f61f54268e98418b38da02e7
João Paulo <jplbrasil@gmail.com>
2022-10-31 19:57:35 -0300

    Update banco_dados.php
    Adicionado bloco para conexao em banco de dados SQLite

--- banco_dados.php
+++ banco_dados.php
@@ -1,6 +1,6 @@
 <?php
 	
-	// Dados para conexao em base de dados local
+	// Dados para conexao em base de dados local MySQL
 	/*
 	$servername = "localhost";
 	$username = "root";
@@ -8,20 +8,25 @@
 	$dbname = "pizzaria";
 	//*/
 
-	// Dados para conexao 'a base de dados da nuvem Heroku, aplicativo eu-digital-pizzaria
-	
+	// Dados para conexao 'a base de dados MySQL da nuvem Heroku, aplicativo eu-digital-pizzaria
+	/*
 	$servername = "us-cdbr-east-04.cleardb.com";
 	$username = "bff01010138ab1";
 	$password = "e04e9d48";
 	$dbname = "heroku_264338b10d713e0";
 	//*/
 				
-	// Create connection
+	// Create connection MySQL
+	/*
 	$conn = new mysqli($servername, $username, $password, $dbname);
 
 	// Check connection
 	if ($conn->connect_error) {
 		die("Erro na conexão com banco de dados: " . $conn->connect_error . "\n");
 	}
+	//*/
 	//echo "Conexão com o banco de dados realizada com sucesso! (base de dados = " . $dbname . ")\n";
+
+	//Dados para conexao local em base de dados SQLite
+	$conn = new SQLite3('localdb.db');
 ?>
